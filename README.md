90090b6650f18f5c3e3c794d2bba85aabae892e2
Jan Lunge <janlunge1993@gmail.com>
2023-01-08 00:51:03 +0100

    added instructions

---
+++ README.md
@@ -0,0 +1,25 @@
+# Karabiner Chord manager
+
+# dependencies
+you need to have `node` and `yarn` installed
+
+
+to install run
+```
+yarn install
+```
+
+then to add a chord run
+```
+node index.js --from "qwe" --to "text for qwe chord"
+```
+
+or the shorthand with
+```
+node index.js -f "qwe" -t "text for qwe chord"
+```
+
+then to write this to karabiner just run
+```
+node index.js -w
+```
--- index.js
+++ index.js
@@ -8,8 +8,16 @@
 import JSON5 from 'json5'
 import fs from 'fs'
 import {homedir} from 'os'
+import commandLineArgs from 'command-line-args'
 const chords = JSON5.parse(fs.readFileSync('./chords.json'))
+const optionDefinitions = [
+    { name: 'add', alias: 'a', type: Boolean },
+    { name: 'update', alias: 'w', type: Boolean },
+    { name: 'from', alias: 'f', type: String },
+    { name: 'to', alias: 't', type: String },
 
+  ]
+const options = commandLineArgs(optionDefinitions)
 const generateFile = () => {
     const json = {
         title: 'wChording',
@@ -44,21 +52,39 @@ const generateFile = () => {
     })
     return json
 }
-const generatedChords = generateFile()
-const generatedChordsString = JSON.stringify(generatedChords, null,4)
-// write to this folder
-fs.writeFileSync('./wChording.json',generatedChordsString)
-// write to karabiner comblex mods
-fs.writeFileSync(homedir+'/.config/karabiner/assets/complex_modifications/wChording.json',generatedChordsString, { flag: 'w' })
-// replace in the karabiner.json
-const karabinerjson = JSON.parse(fs.readFileSync(homedir+'/.config/karabiner/karabiner.json'))
-const clearedKjson = {
-    ...karabinerjson,
+
+// Write file to Karabiner
+const writeFileToKarabiner = () => {
+    const generatedChords = generateFile()
+    const generatedChordsString = JSON.stringify(generatedChords, null,4)
+    // write to this folder
+    fs.writeFileSync('./wChording.json',generatedChordsString)
+    // write to karabiner comblex mods
+    fs.writeFileSync(homedir+'/.config/karabiner/assets/complex_modifications/wChording.json',generatedChordsString, { flag: 'w' })
+    // replace in the karabiner.json
+    const karabinerjson = JSON.parse(fs.readFileSync(homedir+'/.config/karabiner/karabiner.json'))
+    const clearedKjson = {
+        ...karabinerjson,
+    }
+    clearedKjson.profiles[0].complex_modifications.rules = karabinerjson.profiles[0].complex_modifications.rules.filter(a=>a.description !== 'wChording - Chords');
+
+    // add the new thing to the list
+    clearedKjson.profiles[0].complex_modifications.rules = clearedKjson.profiles[0].complex_modifications.rules.concat(generatedChords.rules) // TODO: not only add first rule
+    fs.writeFileSync(homedir+'/.config/karabiner/karabiner.json', JSON.stringify(clearedKjson,null,4), {flag:'w'})
+
+    console.log("output located at wChording.json");
 }
-clearedKjson.profiles[0].complex_modifications.rules = karabinerjson.profiles[0].complex_modifications.rules.filter(a=>a.description !== 'wChording - Chords');
 
-// add the new thing to the list
-clearedKjson.profiles[0].complex_modifications.rules = clearedKjson.profiles[0].complex_modifications.rules.concat(generatedChords.rules) // TODO: not only add first rule
-fs.writeFileSync(homedir+'/.config/karabiner/karabiner.json', JSON.stringify(clearedKjson,null,4), {flag:'w'})
+// console.log(options);
 
-console.log("output located at wChording.json");
+// add chord
+if(options.from && options.to){
+    const chordlist = JSON5.parse(fs.readFileSync('./chords.json'))
+    chordlist.push({from: options.from, to: options.to})
+    fs.writeFileSync('./chords.json',JSON.stringify(chordlist,null, 4))
+    console.log('added chord');
+}
+
+if(options.update){
+    writeFileToKarabiner()
+}
